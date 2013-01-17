27666dd54dabeabbd25942997424d46ca1d24471
Thijs Treffers <thijs-treffers@hotmail.com>
2013-01-17 15:04:57 +0100

    Server.js is now working with Express 3.0

--- RealTime/server.js
+++ RealTime/server.js
@@ -1,24 +1,11 @@
 var io = require('socket.io');
 var express = require('express');
 
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = express()
+  , server = require('http').createServer(app)
+  , io = io.listen(server);
 
-app.listen(80);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+server.listen(80);
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
