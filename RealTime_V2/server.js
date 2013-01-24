f884a453d2d14e19fad51b1e19864783238fa4d0
Thijs Treffers <thijs-treffers@hotmail.com>
2013-01-24 15:45:43 +0100

    Made a start with the server.
    When you start the server, it starts on http://localhost/ and on port
    80. When you try to connect via index.html, the server logs the username
    (default: Zadkine) and a ClientID, which is generated.

---
+++ RealTime_V2/server.js
@@ -0,0 +1,33 @@
+var app = require('express')()
+	, server = require('http').createServer(app)
+	, io = require('socket.io').listen(server);
+
+chatClients = new Object();
+
+server.listen(80);
+
+io.set('log level', 2);
+
+io.sockets.on('connection', function(socket) {
+	socket.on('user_connected', function(data){
+		connect(socket, data);
+	})
+});
+
+function connect(socket, data){
+	data.clientID = generateID();
+
+	chatClients[socket.id] = data;
+
+	console.log(data);
+}
+
+function generateID(){
+  var S4 = function () {
+    return (((1 + Math.random()) * 0x100000) |
+      0).toString(16).substring(1);
+  };
+
+  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" +
+    S4() + "-" + S4() + S4() + S4());
+}
