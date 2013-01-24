0bd1df963a22dfab9a6a2832946be50cb3284d41
Vincent Molenaar <vincent_molenaar7@hotmail.com>
2013-01-24 11:13:51 +0100

    update mobile

--- Mobile/EnergyVampire/src/com/patchingzone/energyvampire/MainActivity.java
+++ Mobile/EnergyVampire/src/com/patchingzone/energyvampire/MainActivity.java
@@ -1,6 +1,14 @@
 package com.patchingzone.energyvampire;
 
+import java.io.BufferedReader;
 import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -60,6 +68,30 @@ public class MainActivity extends Activity {
 	{
 		Toast toast = Toast.makeText(getApplicationContext(), "Connecting", Toast.LENGTH_SHORT);
 		toast.show();
+		
+		/*
+		 try {
+	            Socket s = new Socket("10.250.89.28", 80);	            //outgoing stream redirect to socket
+	            OutputStream out = s.getOutputStream();
+	            PrintWriter output = new PrintWriter(out);
+	            output.println("Hello Android!");
+	            BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	            //read line(s)
+	            String st = input.readLine();
+	            //Close connection
+	            s.close();
+				Log.e("con", "ok" + st + ": "+ input);
+	    } catch (UnknownHostException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	            Log.e("con", "f1");
+
+	    } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	            Log.e("con", "f2" + e );
+	    }
+*/
 		info.setText("Click I'm Ready if your ready.");
 		BT_home.setText("I'm Ready");
 		
@@ -79,20 +111,41 @@ public class MainActivity extends Activity {
 		     }
 
 		     public void onFinish() {
-		         Color();
+		    	 int Min = 1;
+		    	 int Max = 4;
+		    	 int rndNum = (int) (Math.random() * ( Max - Min ));
+		         Color(rndNum);
 		     }
 		  }.start();
 	}
 	
-	public void  Color()
+	public void  Color(final int color)
 	{
 		count.setVisibility(View.GONE);
 		
-		new CountDownTimer(2550, 1) {
+		// 20 = 5.10 sec
+		// 10 = 2.55 sec
+		final int multi = 20;
+		new CountDownTimer((255 * multi), 1) {
 
-		     public void onTick(long millisUntilFinished) {
-		         body.setBackgroundColor(android.graphics.Color.rgb(((int)millisUntilFinished / 10),0,0));
-		        //Log.e("haas", ""+millisUntilFinished);  
+			
+		     public void onTick(long millisUntilFinished) 
+		     {		    	 
+		    	 switch(color)
+		    	 {
+	    	 		case 1:
+	    	 			body.setBackgroundColor(android.graphics.Color.rgb(((int)millisUntilFinished / multi),0,0)); 
+		    		break;
+	    	 		case 2:
+	    	 			body.setBackgroundColor(android.graphics.Color.rgb(0,((int)millisUntilFinished / multi),0)); 
+    	 			break;
+	    	 		case 3:
+	    	 			body.setBackgroundColor(android.graphics.Color.rgb(0,0,((int)millisUntilFinished / multi))); 
+    	 			break;
+	    	 		case 4:
+	    	 			body.setBackgroundColor(android.graphics.Color.rgb(((int)millisUntilFinished / multi),0,((int)millisUntilFinished / multi))); 
+    	 			break;
+		    	 }		         
 		     }
 
 		     public void onFinish() {
