6adb7d41c02e89f38382026b3d05e414235b6e42
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2013-01-31 11:47:41 +0100

    Player.js fixed and working

--- RealTime_V2/Player.js
+++ RealTime_V2/Player.js
@@ -1,46 +1,36 @@
-function Player(id, username, lng, lat, score, team, nickname) {
-	var playerId = id,
-		username = username,
-		lng = lng,
-		lat = lat,
-		score = score,
-		team = team,
-		nickname = nickname;
-		isReady = false;
 
-		function addScore(value)
-		{
-			score += value;
-		}
 
-		function drainScore(value)
-		{
-			score -= value;
-		}
-
-		function switchTeam(team)
-		{
-			team = team;
-		}
-
-		function updateLocation(location)
-		{
-			lng = location.lng;
-			lat = location.lat;
-		}
-
-		function getLocationPoint()
-		{
-			return "{" + lng + "," + lat + "}";
-		}
-
-		function getId() { return playerId; }
-		function getUsername() { return username; }
-		function getLng() { return lng; }
-		function getLat() { return lat; }
-		function getScore() { return score; }
-		function getTeam() { return team; }
-		function getNickname() { return nickname; }
-		function setNickname(value) { nickname = value; }
-		function getIsReady() { return isReady; }
+function Player (id, nickname, lng, lat, score, team) { 
+	this.playerId = id;
+	this.nickname = nickname,
+	this.lng = lng,
+	this.lat = lat,
+	this.score = score,
+	this.team = team,	
 }
+
+
+Player.prototype.addScore = function addScore(value){ this.score += value; };
+Player.prototype.drainScore = function drainScore(value){ this.score -= value; };
+Player.prototype.switchTeam = function switchTeam(team){ this.team = team; };
+Player.prototype.updateLocation = function updateLocation(location){ this.lng = location.lng; this.lat = location.lat;};
+Player.prototype.getLocationPoint = function getLocationPoint(){return "{" + this.lng + "," + this.lat + "}";};
+
+//getters and setters
+
+Player.prototype.getId 			=	function getId(){ return this.playerId; };
+Player.prototype.getNickname 	=	function getNickname(){ return this.nickname;	 };
+Player.prototype.setNickname 	=	function setNickname(value){ this.nickname = value; };
+Player.prototype.getLng 		= 	function getLng(){ return this.lng; };
+Player.prototype.getLat 		=	function getLat(){ return this.lat; };
+Player.prototype.getScore 		=	function getScore(){ return this.score; };
+Player.prototype.getTeam 		=	function getTeam(){ return this.team; };
+Player.prototype.getUsername 	=	function getUsername(){ return this.username; };
+
+
+
+
+
+
+
+exports.Player = Player;
