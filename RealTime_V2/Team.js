46dc1656d289addddbca084e4c3ff55598c1e786
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2013-02-14 10:39:45 +0100

    an var name in team js has changed name

--- RealTime_V2/Team.js
+++ RealTime_V2/Team.js
@@ -1,12 +1,12 @@
 var Team = function Team(id, name, color)
 {
 	this.id = id;
-	this.name = name;
+	this.teamName = name;
 	this.color = color;
 }
 
 Team.prototype.getID = function getID(){ return this.id; };
-Team.prototype.getName = function getName(){ return this.name; };
+Team.prototype.getTeamName = function getName(){ return this.teamName; };
 Team.prototype.getColor = function getColor(){ return this.color; };
 
 exports.Team = Team;
